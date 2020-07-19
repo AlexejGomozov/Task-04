c18436635069c07c1de7f67e932008953d28a800
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-19 17:44:27 +0200

    Update Task19.java

--- Task19.java
+++ Task19.java
@@ -6,37 +6,37 @@ import java.util.Scanner;
   Write a program that determines the sum of n - digit numbers containing only odd digits. Also determine 
   how many even digits are in the found amount. To solve the problem, use decomposition. */
 
-                                      public class Task19 {
+                                       public class Task19 {
 
 
-                                 public static int [] oddNumb (int x) {                 //находим массив случайных чисел с количеством ячеек "x"
+                                    public static int [] oddNumb (int x) {                 //находим массив случайных чисел с количеством ячеек "x"
 		                                                                       
-	                          int [] arrayOddNumb = new int[x];
+	                         int [] arrayOddNumb = new int[x];
 	
 	                       for( int i = 0; i< x; i++) {
 		
-		                arrayOddNumb[i] =(int) (Math.random()*10) ;
+		            arrayOddNumb[i] =(int) (Math.random()*10) ;
 		
-		             if(arrayOddNumb[i]%2 == 0 | arrayOddNumb[i] == 0) arrayOddNumb[i] = arrayOddNumb[i] + 1;  //определяем, чтобы эти числа не были равны "0" и четным цифрам
+		         if(arrayOddNumb[i]%2 == 0 | arrayOddNumb[i] == 0) arrayOddNumb[i] = arrayOddNumb[i] + 1;  //определяем, чтобы эти числа не были равны "0" и четным цифрам
 				
 	               }
 	             return arrayOddNumb;
-               }
+                   }
                  public static int figureOdd (int [] x) {                          //из массива выводим одно число, объеденив ячейки через String
 		
-	                String sumString = ""; //null;
+	              String sumString = ""; //null;
 	              
 	                   for(int variable: x ){
 	                	
-		                  sumString  = sumString + ( variable);	
-                            }
-	                         int sum = Integer.parseInt(sumString);                   //полученное число из String переводим в int
+		                sumString  = sumString + ( variable);	
+                                  }
+	                            int sum = Integer.parseInt(sumString);                   //полученное число из String переводим в int
 	
-	                            return sum;
-                                 }
-                                 public static int evenNumb( int y) {                 //определяем сколько четных цифр в полученной сумме, для этого проверяем каждую цифру
+	                              return sum;
+                                       }
+                                       public static int evenNumb( int y) {                 //определяем сколько четных цифр в полученной сумме, для этого проверяем каждую цифру
 	
-	                           String StringY = String.valueOf(y);
+	                            String StringY = String.valueOf(y);
 	                           
 	                         int evenN = 0;
 	
@@ -44,37 +44,37 @@ import java.util.Scanner;
 	
 	                    for (int i = 1; i <=StringY.length(); i++) {
 		
-		             evenN = y % 10;  
+		          evenN = y % 10;  
 		             
-		          if (evenN % 2 == 0 & evenN != 0)   count ++;              
+		       if (evenN % 2 == 0 & evenN != 0)   count ++;              
 		          
-		        y = y/10;
-	          }	                    
-	        return count;
-          }
-          public static void task() {
+		     y = y/10;
+	            }	                    
+	         return count;
+                }
+                public static void task() {
 	
-	        int n, m, c, v;
+	          int n, m, c, v;
 	
-	          System.out.print("Для решения данной задачи n-значное число может быть от 2 до 9.\n " +
-	           "Введите число для определения n - значных чисел >> ");
+	            System.out.print("Для решения данной задачи n-значное число может быть от 2 до 9.\n " +
+	              "Введите число для определения n - значных чисел >> ");
 	
-	             Scanner sc = new Scanner(System.in);
+	                 Scanner sc = new Scanner(System.in);
 	             
-	               v = sc.nextInt();
+	                    v = sc.nextInt();
 	
-	                System.out.println("v = " + v);
+	                      System.out.println("v = " + v);
 	
-                      n = figureOdd (oddNumb (v));
+                                 n = figureOdd (oddNumb (v));
 	
-	                    m = figureOdd (oddNumb (v));
+	                            m = figureOdd (oddNumb (v));
 	
-	                      c = n + m; 
+	                               c = n + m; 
 	
-	                        System.out.println("Два числа, содержащие нечетные цифры :" + n + ", "+ m);
+	                                 System.out.println("Два числа, содержащие нечетные цифры :" + n + ", "+ m);
 	
-	                          System.out.println("Сумма двух нечетных чисел  = "+ c + ". Количество четных цифр в сумме данных чисел = " + evenNumb(c));
+	                                    System.out.println("Сумма двух нечетных чисел  = "+ c + ". Количество четных цифр в сумме данных чисел = " + evenNumb(c));
 
-	                            sc.close();
-                                  }
-                                    }
+	                                      sc.close();
+                                                }
+                                                  }
